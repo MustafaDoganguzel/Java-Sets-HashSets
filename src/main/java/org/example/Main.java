ad63a4a6d3f5ee9b20aa6c90e157ef02936cf1de
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-03-28 17:26:04 +0300

    Fix imports and clean up unused dependencies.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.entity.Priority;
+import org.example.entity.Status;
+import org.example.entity.Task;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    Task ann = new Task("deneme1", "deneme1", "ann",Status.ASSIGNED , Priority.HIGH );
+    Task bob = new Task("deneme2", "deneme2", "bob", Status.ASSIGNED , Priority.HIGH );
+
+    Set<Task> tasks = new HashSet<>();
+
     }
+
 }

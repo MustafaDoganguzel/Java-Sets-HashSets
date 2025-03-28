040d5b13ce34b52de0885bd1a2c37ef8b810d457
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-03-28 17:25:21 +0300

    Fix constructor, method logic, and duplicate variable declarations in TaskData class.

--- src/main/java/org/example/entity/TaskData.java
+++ src/main/java/org/example/entity/TaskData.java
@@ -4,26 +4,58 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class TaskData {
-private Set<Task> annsTasks;
-private Set<Task> bobsTasks;
-private Set<Task> carolTasks;
-private Set<Task> unassignedTasks;
+    private Set<Task> annsTasks = new HashSet<>();
+    private Set<Task> bobsTasks = new HashSet<>();
+    private Set<Task> carolsTasks = new HashSet<>();
+    private Set<Task> unassignedTasks = new HashSet<>();
 
-    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolTasks, Set<Task> unassignedTasks) {
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
         this.annsTasks = annsTasks;
         this.bobsTasks = bobsTasks;
-        this.carolTasks = carolTasks;
+        this.carolsTasks = carolsTasks;
         this.unassignedTasks = unassignedTasks;
     }
 
-    public Set<Task> getTasks(String name){
-    if(name.equals("ann")){
+    public Set<Task> getTasks(String name) {
+        if (name.equals("ann")) {
+            return annsTasks;
+        } else if (name.equals("bob")) {
+            return bobsTasks;
+        } else if (name.equals("carol")) {
+            return carolsTasks;
+        }
+        return unassignedTasks;
+    }
+    public Set<Task> getUnion(Set<Task> task1,Set<Task> task2 ){
+        Set<Task> taskCopy = new HashSet<>(task1);
+        taskCopy.addAll(task2);
+        return  taskCopy;
+    }
+    public Set<Task> getIntersection(Set<Task> task1,Set<Task> task2 ){
+        Set<Task> taskCopy = new HashSet<>(task1);
+        taskCopy.retainAll(task2);
+        return  taskCopy;
+    }
+
+    public Set<Task> getDifferences(Set<Task> task1, Set<Task> task2){
+        Set<Task> taskCopy = new HashSet<>(task1);
+        taskCopy.removeAll(task2);
+        return taskCopy;
+    }
+
+    public Set<Task> getAnnsTasks() {
         return annsTasks;
-    } else if (name.equals("bobs")) {
+    }
+
+    public Set<Task> getBobsTasks() {
         return bobsTasks;
-    } else if (name.equals("carol")){
-        return carolTasks;
-    } else
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
         return unassignedTasks;
     }
 }
